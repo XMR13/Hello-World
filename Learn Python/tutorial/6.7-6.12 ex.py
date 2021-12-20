10669b956600c7280d2c757601cde08b4f523f5d
XMR13 <86971568+XMR13@users.noreply.github.com>
2021-12-20 23:00:53 +0700

    Update 6.7-6.12 ex.py

--- Learn Python/tutorial/6.7-6.12 ex.py
+++ Learn Python/tutorial/6.7-6.12 ex.py
@@ -1,1 +1,28 @@
 #jabaran 6.7
+#6-7. People: Start with the program you wrote for Exercise 6-1 (page 99).
+##Make two new dictionaries representing different people, and store all three
+#dictionaries in a list called people. Loop through your list of people. As you
+#loop through the list, print everything you know about each person.
+
+orang_Rizieq ={
+    "first":"Muhamammad",
+    "last":"Rizaldi",
+    "Umur":19
+}
+
+orang_Martinelli = {
+    "first":"Gabriel",
+    "last":"Martinelli",
+    "Umur":20,
+}
+
+orang_Sumitro = {
+    "first":"Emile",
+    "last":"Rowe",
+    "Umur":20,
+}
+
+nama2 = [orang_Rizieq,orang_Martinelli,orang_Sumitro]
+
+for data in nama2:
+    print(data)
